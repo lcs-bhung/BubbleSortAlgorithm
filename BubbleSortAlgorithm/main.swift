4d4e808c67e0931d24ab68343a4deaeff63e7fe9
Branton Hung <BHung@lcs.on.ca>
2021-03-19 15:09:06 -0400

    Bubble sort works with most optimizations

--- BubbleSortAlgorithm/main.swift
+++ BubbleSortAlgorithm/main.swift
@@ -19,9 +19,11 @@ func waitForInput() {
 
 var dataSet: [Int] = []
 
+
 //populate the array
 
 for _ in 1...10 {
+    
     dataSet.append(Int.random(in: 1...100))
 }
 
@@ -30,38 +32,50 @@ print("unsorted:")
 print(dataSet)
 waitForInput()
 
+var unsorted = dataSet.count - 1
+
 //Sort the array
 
+var swap = false
 
 //loop thriugh the entire array "n times
 for i in 0..<dataSet.count {
+    // pass through the array to float the highest number to the end
+    for j in 0..<unsorted {
+        
+        swap = false
 
-    //1 pass through the array to float the highest number to the end
-    for j in 0..<dataSet.count - 1 {
-
+        //What comparison is this
+        print("pass \(i + 1), comparison \(j + 1)...")
+        
         //compare left value to rught value
         if dataSet[j] > dataSet[j+1] {
-            
+           swap = true
             //Swap valies when leftvalue is greater than the right value
             let temporaryValue = dataSet[j]
             dataSet[j] = dataSet[j + 1]
             dataSet[j + 1] = temporaryValue
             
-            
-
-            
-            
         }
-        
+       
     }
 
+    if swap == false {
+        break
+    }
+    
+    unsorted -= 1
+    
 
     //Print the array after "n" passes
     print("Array after pass \(i+1)")
     print(dataSet)
     waitForInput()
 
-}
+    
+    }
+    
+
 
 //Print the array
 print("sorted:")
